796b6f0af76ce9f6827b17eba740fcc4d9301a7a
GILBERTSAJA <93893614+Gilbertgeraldo@users.noreply.github.com>
2021-12-16 18:43:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const loading = setTimeout(() => {
 }, 5000)
 // END OFF LOADING ANIMATION
 
-CFonts.say("Nana-Md", {
+CFonts.say("Gilbert-Bot", {
   font: "block",
   align: "center",
   gradient: ["red", "blue"],
