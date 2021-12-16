f75dfac229592ed7de7b1c4c6b7b1f7b9af9ff45
GILBERTSAJA <93893614+Gilbertgeraldo@users.noreply.github.com>
2021-12-16 19:35:31 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,10 +2,10 @@ const PhoneNumber = require('awesome-phonenumber')
 async function handler(m) {
                 let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
                     + 'VERSION:3.0\n' 
-                    + 'N:;Adul Alhy;;;'
-                    + 'FN:Adul Alhy\n' // full name
-                    + 'ORG:Adul Alhy;\n' // the organization of the contact
-                    + 'TEL;type=CELL;type=VOICE;waid=6281390199407:+62 813-9019-9407\n' // WhatsApp ID + phone number
+                    + 'N:;Gilbert;;;'
+                    + 'FN:Gilbert geraldo\n' // full name
+                    + 'ORG:Gilbert geraldo;\n' // the organization of the contact
+                    + 'TEL;type=CELL;type=VOICE;waid=6281235136510:+62 812-3513-6510\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
                 conn.sendMessage(m.chat, { contacts: { displayName: 'Adul Alhy', contacts: [{ vcard }] } }, { quoted: m })
 }
