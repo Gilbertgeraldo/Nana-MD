d39a50f8297756290cd30f27b70242046fbc4c5c
GILBERTSAJA <93893614+Gilbertgeraldo@users.noreply.github.com>
2021-12-16 19:40:21 +0700

    Update runtime.js

--- plugins/runtime.js
+++ plugins/runtime.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
 
 m.reply(`
 ┌─〔 R U N T I M E 〕
-├ Bot Aktif Selama ${uptime}
+├ Bot Aktif Selama ${uptime} jangan lupa follow ig owner:gaada_apa_apawkwk
 └────
     `.trim())
 }
